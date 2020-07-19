8cde64d12a51e87bc0ade2ec91246f5d2eebb07a
wangxy <wangxinyang1983@gmail.com>
2020-07-19 23:12:16 +0900

    router修正

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="app">App</div>
+  <div id="app">
+    <router-view />
+    <FooterGuide />
+  </div>
 </template>
-
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
 <style lang="stylus"></style>
